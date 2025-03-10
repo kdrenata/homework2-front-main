02897bc97add11486fac5a6ca65e340e942a9685
kdrenata <kd.renata09@gmail.com>
2025-03-10 15:20:20 +0100

    HW2 done

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -4,30 +4,36 @@ import {AffairType, FilterType} from '../HW2'
 import s from './Affairs.module.css'
 
 type AffairsPropsType = {
-    data: any // need to fix any
-    setFilter: any
-    deleteAffairCallback: any
+    data: Array<AffairType>,
+    setFilter: (filter: FilterType) => void,
+    deleteAffairCallback: (_id: number) => void,
     filter: FilterType
 }
 
 function Affairs(props: AffairsPropsType) {
     const setAll = () => {
-        // need to fix
+        props.setFilter('all')
     }
     const setHigh = () => {
-        // need to fix
+        props.setFilter('high')
     }
     const setMiddle = () => {
-        // need to fix
+        props.setFilter('middle')
     }
     const setLow = () => {
-        // need to fix
+        props.setFilter('low')
     }
+    const filterClasses = {
+        all: s.button + ' all ' + s.all,
+        high: s.button + ' high ' + s.high,
+        middle: s.button + ' middle ' + s.middle,
+        low: s.button + ' low ' + s.low,
+    };
 
-    const cnAll = s.button + ' ' + s.all + (props.filter === 'all' ? ' ' + s.active : '')
-    const cnHigh = s.button + ' ' + s.high + (props.filter === 'high' ? ' ' + s.active : '')
-    const cnMiddle = s.button + ' ' + s.middle + (props.filter === 'middle' ? ' ' + s.active : '')
-    const cnLow = s.button + ' ' + s.low + (props.filter === 'low' ? ' ' + s.active : '')
+    const cnAll = filterClasses.all + (props.filter === 'all' ? ' ' + s.active : '')
+    const cnHigh = filterClasses.high + (props.filter === 'high' ? ' ' + s.active : '')
+    const cnMiddle = filterClasses.middle + (props.filter === 'middle' ? ' ' + s.active : '')
+    const cnLow = filterClasses.low + (props.filter === 'low' ? ' ' + s.active : '')
 
     const mappedAffairs = props.data.map((a: AffairType) => (
         <Affair
